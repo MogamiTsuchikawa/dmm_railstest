f38d8684886f52accdef13206347def488bb1675
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2020-02-03 01:35:58 +0900

    rails入門完了

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '1b19fbbd22abac1c75c573ee9f87649c56f42122b47a2b60124e38492683ff9f4c0d57c328498f75559d2c41e58e8f01404a2b264572f2f8307122f43faf8586'
